db44152adff2a8fe3ac7355b34d1ed2ebc9d6620
Clessio da Silva <clessiosilva44@gmail.com>
2020-03-27 09:14:02 -0300

    status

--- src/test/java/testes/Teste.java
+++ src/test/java/testes/Teste.java
@@ -1,4 +1,4 @@
-package testes;   
+package testes;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -6,12 +6,13 @@ import org.junit.Test;
 //import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebDriver;
 
+import stepsPostivo.CadastroComSucesso;
 import stepsPostivo.Drive;
 import stepsPostivo.Login;
- 
+import stepsPostivo.PesquisaLupa;
+import stepsPostivo.PesquisarPorClick;
 
 public class Teste {
-	
 
 	public WebDriver driver;
 
@@ -19,38 +20,38 @@ public class Teste {
 	public void Abrir() {
 		new Drive();
 		driver = Drive.AbrirNavegador();
+
 	}
+
 	@Test
 	public void FazerLogin() throws Exception {
 		new Login();
 		Login.FazerLogin(driver);
 //		Login.FazerLogin(name, password, driver);
 	}
-	
-	
-//	@Test
-//	public void RealizarCadastro() throws InterruptedException {
-//		new CadastroComSucesso().AbrirMenuUserEIniciarCadastroPositivo(driver);
+
+	@Test
+	public void RealizarCadastro() throws InterruptedException {
+		new CadastroComSucesso().AbrirMenuUserEIniciarCadastroPositivo(driver);
+	}
+
+//	@ParameterizedTest
+//	@ValueSource(strings = {"Cadastro02,","Cadastro02"})
+//	public void palindromes(String user, String password) {
+//	    //assertTrue(StringUtils.isPalindrome(candidate));
 //	}
-//	
-//	 
-////	@ParameterizedTest
-////	@ValueSource(strings = {"Cadastro02,","Cadastro02"})
-////	public void palindromes(String user, String password) {
-////	    //assertTrue(StringUtils.isPalindrome(candidate));
-////	}
-//
-//	
-//	@Test
-//	public void ClickProduto() {
-//		new PesquisarPorClick();
-//		//driver = PesquisarPorClick.ClicarCampoPesquisa();
-//		PesquisarPorClick.ClicarCampoPesquisa(driver);
-//	}
-//	@Test
-//	public void PesquisarComLupa() {
-//		new stepsPostivo.PesquisaLupa();
-//		PesquisaLupa.ClicarCampoPesquisa(driver);
-//	}
-//	
+
+	@Test
+	public void ClickProduto() {
+		new PesquisarPorClick();
+		// driver = PesquisarPorClick.ClicarCampoPesquisa();
+		PesquisarPorClick.ClicarCampoPesquisa(driver);
+	}
+
+	@Test
+	public void PesquisarComLupa() {
+		new stepsPostivo.PesquisaLupa();
+		PesquisaLupa.ClicarCampoPesquisa(driver);
+	}
+
 }
